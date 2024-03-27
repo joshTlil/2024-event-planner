97d126abc8a7fa025fe5f772df5fe7f629322e6a
JoshuaT <jttsat170@gmail.com>
2024-03-27 10:33:19 -0400

    adding time blocks

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 let timeDisplay = document.getElementById('time-display');
+// let hour =  document.getElementById('four');
 
 
 function displayTime(){
     let rightNow = moment().format('hh:mm:ss a');
     timeDisplay.textContent = rightNow;
 }
+
+// let thirdHour = moment().format('h');
+// console.log(thirdHour);
+// console.log(hour.innerText);
+
+// if(hour.innerText === thirdHour){
+//     console.log(true);
+//     console.log("Hello")
+// }
 
 setInterval(displayTime, 1000);
